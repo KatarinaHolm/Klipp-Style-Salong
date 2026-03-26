8e890743e3fcb34cf56efc6b0c09ef8956f0c3f4
Katarina Holm <katarinaholm89@gmail.com>
2026-03-26 14:49:21 +0100

    remove comments in program.cs

--- Program.cs
+++ Program.cs
@@ -12,16 +12,7 @@ namespace Klipp_StyleSalong
 {
     public class Program
     {
-        // Frågor:
-        // Telefonnummer attributet kollade inte att det var tillräckligt många siffror, kan man göra det på nåt sätt? 
-        // Är det korrekt att ta emot id för att ta bort bokning?
-        // Vad ska man skriva i en ReadMe och hur strukturerar man den?
-        // I vilket steg ska man ta bort appsettings.json och skapa repo i GitHub? Blev problem då jag hade skrivit in ConnectionString och skapade repo lokalt.
-        // Bör man lägga till nån kontroll för skip/take eller ha med andra parametrar för paginering?
-        // *AI-förslag : Sätt ett tak: Begränsa alltid pageSize (t.ex. max 100) så att en användare inte kan krascha servern genom att be om en miljon rader.
-        // *AI-förslag: Returnera Metadata: Skicka med information i svaret om hur många sidor som finns totalt, så att frontend vet hur många knappar de ska rita ut.
-
-        public static void Main(string[] args)
+            public static void Main(string[] args)
         {           
             var builder = WebApplication.CreateBuilder(args);
 
